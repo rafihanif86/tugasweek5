396253eb031037fb8a6b973cd903dfb178d6b5fb
Rafi Hanif <rafizmujahid86@gmail.com>
2019-03-12 05:51:33 +0700

    second commit

--- app/src/main/java/rafihanif/registration/MainActivity.java
+++ app/src/main/java/rafihanif/registration/MainActivity.java
@@ -1,13 +1,21 @@
 package rafihanif.registration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE_nama = "rafihanif.registration.extra.MESSAGE";
+    public static final String EXTRA_MESSAGE_alamat = "rafihanif.registration.extra.MESSAGE";
+    public static final String EXTRA_MESSAGE_nohp = "rafihanif.registration.extra.MESSAGE";
+    private EditText mMessageEditText_name;
+    private EditText mMessageEditText_alamat;
+    private EditText mMessageEditText_nohp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, secondActivity.class);
+        startActivity(intent);
+        mMessageEditText_name = (EditText) findViewById(R.id.nama);
+        String message_name = mMessageEditText_name.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE_nama, message_name);
+        startActivity(intent);
+        mMessageEditText_alamat = (EditText) findViewById(R.id.alamat);
+        String message_alamat = mMessageEditText_alamat.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE_alamat, message_alamat);
+        startActivity(intent);
+        mMessageEditText_nohp = (EditText) findViewById(R.id.nohp);
+        String message_nohp = mMessageEditText_nohp.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE_nohp, message_nohp);
     }
 }
--- app/src/main/java/rafihanif/registration/secondActivity.java
+++ app/src/main/java/rafihanif/registration/secondActivity.java
@@ -1,7 +1,9 @@
 package rafihanif.registration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class secondActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class secondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message_nama = intent.getStringExtra(MainActivity.EXTRA_MESSAGE_nama);
+        TextView textView_nama = (TextView) findViewById(R.id.nama_message);
+        textView_nama.setText(message_nama);
+        String message_alamat = intent.getStringExtra(MainActivity.EXTRA_MESSAGE_alamat);
+        TextView textView_alamat = (TextView) findViewById(R.id.alamat_message);
+        textView_alamat.setText(message_alamat);
+        String message_nohp = intent.getStringExtra(MainActivity.EXTRA_MESSAGE_nohp);
+        TextView textView_nohp = (TextView) findViewById(R.id.nohp_message);
+        textView_nohp.setText(message_nohp);
     }
 }
